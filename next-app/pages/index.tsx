775c978cb9c180ed31ab62d7214eefb4f5ea46da
yoshi-non <himekawa.yoshino44@gmail.com>
2022-08-28 17:46:08 +0900

    add: ホームページの追加

--- next-app/pages/index.tsx
+++ next-app/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
@@ -7,7 +8,19 @@ const Home: NextPage = () => {
       <Head>
         <title>Blog StartUp</title>
       </Head>
-      Hello World!
+      <div className='mb-10 p-3 border rounded'>
+        <Image
+          className='object-cover rounded'
+          src="/bg-cafe.jpg"
+          alt='top'
+          width={1280}
+          height={500}
+        />
+      </div>
+      <div className='flex justify-center flex-col items-center mb-10'>
+        <div className='text-lg mb-3'>BLOG POSTS</div>
+        <div className='border w-14'></div>
+      </div>
     </div>
   )
 }
